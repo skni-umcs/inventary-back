70c165e3c86e732d8484c690528c072213787ac9
Buczkek <jakub.buczek@skni.umcs.pl>
2022-04-29 12:54:08 +0200

    Added proper file structure

---
+++ core/routes/endpoints/user.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from fastapi_jwt_auth import AuthJWT
+
+router = APIRouter()
+
+
+@router.get('/')
+def user(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+
+    current_user = Authorize.get_jwt_subject()
+    return {"user": current_user}
+
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
-from pydantic import BaseModel
-from datetime import timedelta
 from fastapi.middleware.cors import CORSMiddleware
+from core import Settings
+from core.routes import api
 
 app = FastAPI()
 
@@ -24,20 +24,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-class User(BaseModel):
-    username: str
-    password: str
-
-
-# in production you can use Settings management
-# from pydantic to get secret key from .env
-class Settings(BaseModel):
-    authjwt_secret_key: str = "totalnyturbogigasekretkoks"
+app.router.include_router(api.api_router)
 
 
 # callback to get your configuration
@@ -56,45 +43,3 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
     )
 
 
-# provide a method to create access tokens. The create_access_token()
-# function is used to actually generate the token to use authorization
-# later in endpoint protected
-@app.post('/login')
-def login(user: User, Authorize: AuthJWT = Depends()):
-    if user.username != "test" or user.password != "test":
-        raise HTTPException(status_code=401, detail="Bad username or password")
-
-    # subject identifier for who this token is for example id or username from database
-    access_token = Authorize.create_access_token(subject=user.username, expires_time=timedelta(minutes=15))
-    refresh_token = Authorize.create_refresh_token(subject=user.username)
-
-    return {"message": "OK",
-            "token": access_token,
-            "refresh_token": refresh_token}
-
-
-# protect endpoint with function jwt_required(), which requires
-# a valid access token in the request headers to access.
-@app.get('/user')
-def user(Authorize: AuthJWT = Depends()):
-    Authorize.jwt_required()
-
-    current_user = Authorize.get_jwt_subject()
-    return {"user": current_user}
-
-
-@app.get("/refresh")
-def refresh(Authorize: AuthJWT = Depends()):
-    Authorize.jwt_refresh_token_required()
-
-    current_user = Authorize.get_jwt_subject()
-    access_token = Authorize.create_access_token(subject=current_user, fresh=False, expires_time=timedelta(days=30))
-
-    return {"token": access_token}
-
-
-@app.post('/logout')
-def logout(Authorize: AuthJWT = Depends()):
-    Authorize.jwt_required()
-
-    return {"msg": "Successfully logout"}
