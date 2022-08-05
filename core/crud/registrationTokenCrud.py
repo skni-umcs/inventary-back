15420f455ae5e399e38f0746929bfa4b74a91675
Buczkek <jakub.buczek@skni.umcs.pl>
2022-08-05 23:54:26 +0200

    Added typing in registration and registration token crud

--- core/crud/registrationTokenCrud.py
+++ core/crud/registrationTokenCrud.py
@@ -2,27 +2,27 @@ from . import Session
 from core.models.registrationTokenModel import RegistrationTokenModel
 
 
-def get_by_id(session: Session, tokenId: int):
+def get_by_id(session: Session, tokenId: int) -> RegistrationTokenModel:
     tokenModel: RegistrationTokenModel = session.query(RegistrationTokenModel).filter(RegistrationTokenModel.id == tokenId).first()
     return tokenModel
 
 
-def get_by_token(session: Session, token: str):
+def get_by_token(session: Session, token: str) -> RegistrationTokenModel:
     tokenModel: RegistrationTokenModel = session.query(RegistrationTokenModel).filter(RegistrationTokenModel.token == token).first()
     return tokenModel
 
 
-def add(session: Session, model: RegistrationTokenModel):
+def add(session: Session, model: RegistrationTokenModel) -> None:
     session.add(model)
     session.commit()
 
 
-def remove(session: Session, model: RegistrationTokenModel):
+def remove(session: Session, model: RegistrationTokenModel) -> None:
     session.delete(model)
     session.commit()
 
 
-def edit(session: Session, editModel: RegistrationTokenModel):
+def edit(session: Session, editModel: RegistrationTokenModel) -> None:
     oldModel: RegistrationTokenModel = session.query(RegistrationTokenModel).filter(RegistrationTokenModel.id == editModel.id).first()
 
     assert oldModel.user_id == editModel.user_id
