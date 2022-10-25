892a905a81d1c849afe5734e2bd21ec935f32214
Buczkek <jakub.buczek@skni.umcs.pl>
2022-10-25 09:43:53 +0200

    Fix imports

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,23 +1,14 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base
 
-from categoryModel import CategoryModel
-from warehouseModel import WarehouseModel
-from userModel import UserModel
-from itemModel import ItemModel
-from privilegesModel import PrivilegesModel
-from registrationModel import RegistrationModel
-from registrationTokenModel import RegistrationTokenModel
-
-
 Base = declarative_base()
 
-# __all__ = [
-#     'categoryModel',
-#     'itemModel',
-#     'privilegesModel',
-#     'userModel',
-#     'warehouseModel',
-#     'registrationTokenModel',
-#     'registrationModel'
-# ]
+# TODO - change table names to plural
+from .categoryModel import CategoryModel
+from .warehouseModel import WarehouseModel
+from .userModel import UserModel
+from .itemModel import ItemModel
+from .privilegesModel import PrivilegesModel
+from .registrationModel import RegistrationModel
+from .registrationTokenModel import RegistrationTokenModel
+
